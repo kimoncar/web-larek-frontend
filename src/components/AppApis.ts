f5c4693fb679dd14b85265ba118519edff5ffdfa
kimoncar <kimoncar@yandex.ru>
2024-08-29 15:16:58 +0300

    Add class AppApis

--- src/components/AppApis.ts
+++ src/components/AppApis.ts
@@ -1,16 +1,24 @@
 import { IApi, IOrder, IOrderResult, IProduct } from "../types";
+import { ApiListResponse } from "./base/api";
 
 export class AppApi {
     private _baseApi: IApi;
+    readonly cdn: string;
 
-	constructor(baseApi: IApi) {
+	constructor(cdn:string, baseApi: IApi) {
 		this._baseApi = baseApi;
+        this.cdn = cdn;
 	}
 
     getProducts(): Promise<IProduct[]> {
-        return this._baseApi.get<IProduct[]>('/product').then((products: IProduct[]) => products);
+        return this._baseApi.get<ApiListResponse<IProduct>>('/product').then((data:ApiListResponse<IProduct>) => 
+            data.items.map((item) => ({
+                ...item,
+                image: this.cdn + item.image,
+              }))
+        );
     }
-
+    
     postOrder(data: IOrder): Promise<IOrderResult> {
         return this._baseApi.post<IOrder>(`/order`, data, 'POST').then((res: IOrderResult) => res);
     }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import { AppApi } from './components/AppApis';
+import { Api } from './components/base/api';
 import './scss/styles.scss';
+import { IApi, IProduct } from './types';
+import { API_URL, CDN_URL, settings } from './utils/constants';
 
+const baseApi: IApi = new Api(API_URL, settings);
+const api = new AppApi(CDN_URL, baseApi);
+
+api.getProducts()
+  .then(function (data: IProduct[]) {
+    console.log(data);
+  })
+  // .then(dataModel.setProductCards.bind(dataModel))
+  .catch(error => console.log(error))
